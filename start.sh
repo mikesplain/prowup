00f6383ad1741be9b38c9c8f95c0a3695043c16f
Mike Splain <mike.splain@gmail.com>
2020-10-18 13:36:12 -0400

    Add kind and start

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kind create cluster --config kind.conf
+# Wait for cluster
+kubectl wait --for=condition=Ready pods -n kube-system --all --timeout=5m
+
+# nginx ingress
+kubectl apply -f ingress.yaml
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=2m
+
+# Prow
+kubectl apply -f prow-starter-s3.yaml
+
+# Smee
+kubectl apply -f smee.yaml
+
+# https://prow.127.0.0.1.nip.io/
